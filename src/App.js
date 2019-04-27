baf51e4b5d34403209b97f49a57b461840fe8b5f
monnmnp <mon.monn@hotmail.com>
2019-04-27 23:47:09 +0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { Button } from 'antd';
+import axios from 'axios'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: '',
+      dialogID: '',
+    }
+  }
+
+  handleHumanClick = (e) => {
+    axios.get(`https://us-central1-nlp-web-e6fd1.cloudfunctions.net/api2/addResult?dialogID=` + this.state.dialogID + `&answer=Human`)
+      .then(res => {
+        console.log(this.state.dialogID)
+      })
+    this.getDialog();
+  }
+
+  handleBotClick = (e) => {
+    axios.get(`https://us-central1-nlp-web-e6fd1.cloudfunctions.net/api2/addResult?dialogID=` + this.state.dialogID + `&answer=Bot`)
+      .then(res => {
+        console.log(this.state.dialogID)
+      })
+    this.getDialog();
+  }
+
+  componentDidMount() {
+    this.getDialog();
+  }
+
+  getDialog() {
+    axios.get(`https://us-central1-nlp-web-e6fd1.cloudfunctions.net/api2/getDialog`)
+      .then(res => {
+        const dialog = res.data.data.dialog;
+        this.setState({
+          text: dialog,
+          dialogID: res.data.dialogID
+        });
+      })
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <div style={{ marginBottom: '20px' }}>คุณคิดว่าข้อความนี้เป็นข้อความจากใคร?</div>
+        <div style={{ marginBottom: '30px', fontSize: '30px' }}>
+          {this.state.text}
+        </div>
+        <Button style={{ marginRight: '20px' }} onClick={this.handleHumanClick}>คน</Button>
+        <Button onClick={this.handleBotClick}>ShishBot</Button>
+      </div>
+    );
+  }
 }
 
 export default App;
